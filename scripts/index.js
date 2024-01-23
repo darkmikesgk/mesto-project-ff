65aff347b288547d9c43f29adf03abea414eb819
Ivan Pidko <vany2008@yandex.ru>
2024-01-23 08:56:41 +0300

    Add 6 cards

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,44 @@
 
 // @todo: DOM узлы
 
+//const cardList = cardConteiner.querySelector('.places__item');
+//console.log(cardList);
+//console.log(deleteButton);
+//const cardDescription = cardList.querySelector('.card__description');
+//console.log(cardDescription);
+//console.log(cardTitle);
+//console.log(likeButton);
+
 // @todo: Функция создания карточки
 
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+//const placesList = document.querySelector('.places__list');
+
+
+function addCard(element, deleteCard) {
+  let cardConteiner = document.querySelector('#card-template').content;
+  let card = cardConteiner.cloneNode(true);
+
+  const cardImage = card.querySelector('.card__image');
+  cardImage.src = element.link;
+  cardImage.atl = element.name;
+  
+  const cardTitle = card.querySelector('.card__title');
+  cardTitle.textContent = element.name;
+
+  const deleteButton = card.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', deleteCard(deleteButton));
+
+  return card;
+}
+
+initialCards.forEach(cardData => {
+  const cardElement = addCard(cardData, ()  => {
+  });
+  document.querySelector('.places__list').appendChild(cardElement);
+});
+
+
